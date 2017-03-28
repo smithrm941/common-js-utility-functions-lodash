09478887d962e9bb38e7106fdcfc30fa583758de
afterthebleep <afterthebleep@gmail.com>
2017-03-28 16:08:59 -0700

    WIP commit. Found floor files initial commit

--- src/math/floor.js
+++ src/math/floor.js
@@ -1,1 +1,35 @@
-"use strict";
+const floor = (number, precision) => {
+    let numberString = number.toString();
+    let decimalIndex = numberString.indexOf('.');
+    let beforeDecimal = numberString.substring(0, decimalIndex);
+    let afterDecimal = numberString.substring(decimalIndex, numberString.length);
+  if (Number.isInteger(number)){
+    return number;
+  } else if (Number.isInteger(number) === false) {
+      if (precision === undefined || precision  === 0) {
+        for(let i = 0; i < afterDecimal.length; i++){
+          if(numberString[i] > 0){
+            let roundDown = parseInt(beforeDecimal) -1;
+            return roundDown;
+          }
+        }
+      } else if (precision > 0) {
+          let beforeDecimalArray = [];
+          beforeDecimalArray.push(beforeDecimal);
+          let afterDecimalArray = afterDecimal.split('');
+          for(let j = 1; j < afterDecimalArray.length; j++){
+            if(j === precision){
+              afterDecimalArray.splice(j, 1, parseInt(afterDecimalArray[j])+1);
+              afterDecimalArray.splice(j, 1, afterDecimalArray[j].toString());
+            }
+          }
+          afterDecimalArray.unshift(beforeDecimalArray[0]);
+          let newNumberString = afterDecimalArray.join('');
+          let roundedFloat = newNumberString.substring(0, decimalIndex + precision + 1);
+          return parseFloat(roundedFloat);
+        }
+    }
+
+}
+
+export { floor }
---
+++ test/floor_test.js
@@ -0,0 +1,22 @@
+import chai from 'chai'
+import { floor } from '../src/math/floor'
+
+let expect = chai.expect
+let should = chai.should()
+
+describe( 'return the smalllst integer less than or equal to the given number', function() {
+  it.only('returns an integer if precision is not specified', function(){
+    expect(floor(4.006)).to.be.equal(4)
+    expect(floor(4.000)).to.be.equal(4)
+  })
+  it('returns an integer if prscision is zero'), function(){
+    expect(floor(5.56789476740, 0)).to.be.equal(5)
+  })
+  it('if the first argument is a float and the second is greater than zero, will return the smallest float less than or equal to the give float with the number of places after the decimal equal to the second argument', function() {
+    expect(floor(7.1141, 2)).to.be.equal(7.11)
+    expect(floor(4.123456, 3)).to.be.equal(4.123)
+  })
+  it('if the resulting float only contains zeros after the decimal, then it returns an integer', function(){
+    expect(floor(7.004, 2)).to.be.equal(7)
+  })
+})
