5ed70d1820e1ba19255d404e2dd0affaec654d1f
aisha 'ai' ortiz <afterthebleep@gmail.com>
2017-03-30 18:04:50 -0700

    deleted unnecessary file

--- test/nth-arg_test.js
+++
@@ -1,8 +0,0 @@
-// var expect = require('chai').expect
-// var nthArg = require('../src/nth-arg')
-//
-// describe('nthArg()', function(){
-//   it('should...', function(){
-//     expect(nthArg(1))
-//   })
-// })
